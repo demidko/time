e6b8ad62050a7a54b39251f5db2c05b39fc6b3df
Daniil Demidko <demidko@farpost.com>
2021-10-09 21:57:12 +1000

    tests fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,14 @@ plugins {
   kotlin("jvm") version "1.5.31"
 }
 dependencies {
-  testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
   testImplementation("com.natpryce:hamkrest:1.8.0.1")
-  testImplementation("io.mockk:mockk:1.11.0")
+  testImplementation("io.mockk:mockk:1.12.0")
 }
 tasks.compileKotlin {
-  kotlinOptions.jvmTarget = "16"
   kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime"
 }
 tasks.compileTestKotlin {
-  kotlinOptions.jvmTarget = "16"
   kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime"
 }
 tasks.test {
